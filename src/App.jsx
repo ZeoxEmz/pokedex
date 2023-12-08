6371d49f95ec2265659da2ee90da4b2530a4a74d
ZeoxEmz <ezequielcaceres98@outlook.com>
2023-12-08 15:19:26 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import { Create } from './components/create/create'
-import { Detail } from './components/detail/detail'
-import { Home } from './components/home/home'
-import { Landing } from './components/landing/landing'
-import { Route, Routes, Router } from 'react-router-dom'
+import './App.css';
+import { Create } from './components/create/create';
+import { Detail } from './components/detail/detail';
+import { Home } from './components/home/home';
+import { Landing } from './components/landing/landing';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function MainApp() {
   return (
     <div className='app'>
       <h1>hola</h1>
-      <Router>
-        <Routes basename="/pokedex">
-          <Route path='/' element={<Landing/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/detail/:id' element={<Detail/>}/>
-          <Route path='/crear' element={<Create/>}/>
+      <Router basename="/pokedex">
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/detail/:id' element={<Detail />} />
+          <Route path='/crear' element={<Create />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default MainApp 
+export default MainApp;
