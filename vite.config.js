3f448d2e39c2ebaec3b4b5f7f26eab044c9050a1
ZeoxEmz <ezequielcaceres98@outlook.com>
2023-12-08 10:48:00 -0300

    h1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/pokedex/',
+  base: './',
   plugins: [react()],
   build: {
     assetsDir: 'assets',
