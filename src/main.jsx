25d66719b88b13dcb5d2428ee80ff01287da4f39
ZeoxEmz <ezequielcaceres98@outlook.com>
2023-12-19 19:22:53 -0300

    cambiando router

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import ReactDOM from 'react-dom/client'
-/* import { BrowserRouter } from 'react-router-dom' */
+import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
-
-import {Provider} from "react-redux"
-import store from './redux/store.js'
-import MainApp  from './App.jsx'
-import './index.css'
-
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import MainApp from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <Router basename="/pokedex/">
-        <MainApp  />
+    <Router basename="https://zeoxemz.github.io/pokedex/">
+      <MainApp />
     </Router>
   </Provider>
-)
+);
